8bd67716db8cb9bd1cfc4a9748a97bc452ec0d6a
typej2003 <typej2003@gmail.com>
2025-12-02 02:16:29 -0400

    ac

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Rednet - Diseño Responsive</title>
+    <title>Rednet</title>
     <!-- Enlace a Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <!-- Enlace a Font Awesome para iconos (necesario para el chat y redes sociales) -->
